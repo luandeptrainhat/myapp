621d48df582784b27ce7054476c504ce47132fc8
luandeptrainhat <115625569+luandeptrainhat@users.noreply.github.com>
2023-07-21 13:18:41 +0700

    thêm api

--- routes/lichhoc.js
+++ routes/lichhoc.js
@@ -8,4 +8,25 @@ router.get('/', async function (req, res, next) {
     var data = await modellichhoc.find();
     res.json(data);
   });
+
+  router.get('/user', async function (req, res, next) {
+
+    try {
+      const { id_user } = req.body;
+      //tạo model
+      const userFind = { id_user };
+      var query = {
+        id_user: id_user,
+      }
+      
+      var data= await modellichhoc.find(query);
+      res.json({status: true, message:"Lấy thành công",data:data}); 
+    }
+    catch (error) {
+      res.json({ status: false, message: "Lấy thất bại" });
+    }
+  
+  
+  
+  });
 module.exports = router;
--- routes/lichthi.js
+++ routes/lichthi.js
@@ -5,7 +5,31 @@ var modellichthi = require('../models/lichthi');
 
 
 router.get('/', async function (req, res, next) {
-    var data = await modellichthi.find();
-    res.json(data);
-  });
+  var id_user = req.query.id_user;
+  var data = await modellichthi.findById(id_user);
+  res.json(data);
+});
+
+router.get('/user', async function (req, res, next) {
+
+  try {
+    const { id_user } = req.body;
+    //tạo model
+    const userFind = { id_user };
+    var query = {
+      id_user: id_user,
+    }
+    
+    var data= await modellichthi.find(query);
+    res.json({status: true, message:"Lấy thành công",data:data}); 
+  }
+  catch (error) {
+    res.json({ status: false, message: "Lấy thất bại" });
+  }
+
+
+
+});
+
+
 module.exports = router;
