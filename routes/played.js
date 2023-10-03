0ae295126447fa02d3f6ecb23bc5790eaa5a7678
Mang Tuấn Vĩ <tuanvimang@gmail.com>
2023-10-03 10:24:04 +0700

    update

--- routes/played.js
+++ routes/played.js
@@ -3,42 +3,34 @@ var router = express.Router();
 var modelPlayed = require('../models/played');
 
 router.get('/', async function (req, res, next) {
-    var data = await modelPlayed.find();
-    res.json(data);
+  var data = await modelPlayed.find();
+  res.json(data);
 });
 
-// Route để lọc dữ liệu theo game_id
-router.get('/find-by-game-id/:gameId', async (req, res) => {
-  const gameId = req.params.gameId;
-
+// lọc theo game id
+router.get('/game_id', async function (req, res, next) {
   try {
-    // Sử dụng Mongoose để tìm dữ liệu dựa trên game_id
-    const data = await Played.find({ game_id: gameId });
-
-    if (!data || data.length === 0) {
-      return res.status(404).json({ message: 'Không tìm thấy dữ liệu cho game_id này' });
-    }
-
+    var game_id = req.query.game_id;
+    var data = await modelPlayed.find({ game_id: game_id });
     res.json(data);
+
   } catch (error) {
-    console.error('Lỗi trong quá trình xử lý yêu cầu:', error);
-    res.status(500).json({ message: 'Lỗi trong quá trình xử lý yêu cầu' });
+    res.json({ status: false, message: "Lọc thất bại" });
   }
 });
 
-
 router.post('/lay-diem', async function (req, res, next) {
 
-    try {
-      const { id_user, game_id,diem } = req.body;
-      //tạo model
-      const PlayedInsert = { id_user, game_id,diem};
-      await modelPlayed.create(PlayedInsert);
-      res.json({ status: true, message: 'Thêm user thành công' });
-  
-    }
-    catch (error) {
-      res.json({ status: false, message: "Thêm thất bại" });
-    }
-  });
+  try {
+    const { id_user, game_id, diem } = req.body;
+    //tạo model
+    const PlayedInsert = { id_user, game_id, diem };
+    await modelPlayed.create(PlayedInsert);
+    res.json({ status: true, message: 'Thêm user thành công' });
+
+  }
+  catch (error) {
+    res.json({ status: false, message: "Thêm thất bại" });
+  }
+});
 module.exports = router;
